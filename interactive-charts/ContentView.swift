1932f07ccc785992b3a0349dab5eeba7a13d150b
Wanhar <wanhardaengmaro@gmail.com>
2024-08-07 13:23:04 +0700

    main

---
+++ interactive-charts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  interactive-charts
+//
+//  Created by Wanhar on 07/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ interactive-charts/interactive_chartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactive_chartsApp.swift
+//  interactive-charts
+//
+//  Created by Wanhar on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct interactive_chartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
